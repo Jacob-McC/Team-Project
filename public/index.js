15b823253c0f34c245adb2b26dec345de0cd340f
TheJackOfAllTrade <hendersonshamus03@gmail.com>
2024-11-02 15:40:01 +0000

    Comments
    Added some comments on the variables used in the index.js file.
    
    Just in case anyone wants to work on that file

--- public/index.js
+++ public/index.js
@@ -1,13 +1,14 @@
 "use strict";
 
 window.addEventListener("load", function(){
-    var clickerButton = this.document.getElementsByName("clickerButton")[0];
-    var currencyText = this.document.getElementsByName("currency")[0];
-    var clicked = 0;
+    var clickerButton = this.document.getElementsByName("clickerButton")[0]; //The clicker button
+    var currencyText = this.document.getElementsByName("currency")[0]; //The text for the user's currency
+    var clicked = 0; //Mainly a debug var but is used for displaying the user's money
+    var clickScale = 1; //For if any of the upgrades increase money per click
     console.log("Page Loaded Successfully");
 
 
-    clickerButton.addEventListener("click", function(){
+    clickerButton.addEventListener("click", function(){ //When clicker is clicked it will put money up
         clicked++
         currencyText.textContent = "$ " + clicked;
         console.log("The button has been clicked!!! We are now at: " + clicked + " Clicks!");
