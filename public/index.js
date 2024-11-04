e1e9a5c75b618c8114ab8c18c5fa36f4ee5e265a
TheJackOfAllTrade <hendersonshamus03@gmail.com>
2024-11-04 14:57:49 +0000

    Upgrade (cont.)
    - Added functionality to button upgrades
    - Added pointer change when hovering over buttons

--- public/index.js
+++ public/index.js
@@ -2,11 +2,13 @@
 
 window.addEventListener("load", function(){
     var clickerButton = this.document.getElementsByName("clickerButton")[0]; //The clicker button
-    var upgradeButton1 = this.document.getElementsByName("upgrade1")[0]; //Upgrade 1 button
+    var upgradeButton = this.document.getElementsByClassName("upgradeButton"); //The upgrade buttons
+    var upgradeCost = this.document.getElementsByClassName("upgradeCost");// The cost of each upgrade
     var currencyText = this.document.getElementsByName("currency")[0]; //The text for the user's currency
     var clicked = 0; //Mainly a debug var but is used for displaying the user's money
     var clickScale = 1; //For if any of the upgrades increase money per click
     console.log("Page Loaded Successfully");
+    console.log(upgradeButton);
 
 
     clickerButton.addEventListener("click", function(){ //When clicker is clicked it will put money up
@@ -15,8 +17,13 @@ window.addEventListener("load", function(){
         console.log("The button has been clicked!!! We are now at: " + clicked + " Clicks!");
     })
 
-    upgradeButton1.addEventListener("click", function(){
-        console.log("Upgrade 1 purchased! :) ");
-    })
+    for(let i=0; i < upgradeButton.length; i++){
+        upgradeButton[i].addEventListener("click", function(){
+            console.log("This upgrade costs: " + upgradeCost[i].textContent) 
+            
+            
+            
+            console.log("Upgrade "+ i + " purchased!");
+    })}
 })
 
