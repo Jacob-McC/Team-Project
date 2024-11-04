1f1ea2e1703038da908d978c06dde762967ac303
TheJackOfAllTrade <hendersonshamus03@gmail.com>
2024-11-04 15:23:08 +0000

    Upgrades Owned
    You can now see what upgrades you own
    
    Hooray for Capitalism

--- public/index.js
+++ public/index.js
@@ -6,11 +6,11 @@ window.addEventListener("load", function(){
     var upgradeCost = this.document.getElementsByClassName("upgradeCost"); //The cost of each upgrade
     var upgradeName = this.document.getElementsByClassName("upgradeName"); //The name of each upgrade
     var currencyText = this.document.getElementsByName("currency")[0]; //The text for the user's currency
-    var upgradeOwned = new Array.apply(0, upgradeButton.length);
+    var upgradeOwned = [...Array(upgradeButton.length).fill(0)];
     var clicked = 0; //Mainly a debug var but is used for displaying the user's money
     var clickScale = 1; //For if any of the upgrades increase money per click
     console.log("Page Loaded Successfully");
-    console.log(upgradeButton);
+    console.log(upgradeOwned);
 
 
     clickerButton.addEventListener("click", function(){ //When clicker is clicked it will put money up
@@ -22,7 +22,8 @@ window.addEventListener("load", function(){
     for(let i=0; i < upgradeButton.length; i++){
         upgradeButton[i].addEventListener("click", function(){
             console.log("This upgrade costs: " + upgradeCost[i].textContent) 
-            upgradeName[i].textContent = 
+            upgradeOwned[i]++;
+            upgradeName[i].textContent = "Upgrade " + (i+1) + " (" + upgradeOwned[i] + ")";
             
             
             console.log("Upgrade "+ i + " purchased!");
