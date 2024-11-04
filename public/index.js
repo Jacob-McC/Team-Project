f3e915d2b691c4373ccd369458f643de6c738836
TheJackOfAllTrade <hendersonshamus03@gmail.com>
2024-11-04 14:28:24 +0000

    Added upgrade buttons
    Just the buttons on the right for buying upgrades
    
    M O R E    M O N E Y

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("load", function(){
     var clickerButton = this.document.getElementsByName("clickerButton")[0]; //The clicker button
+    var upgradeButton1 = this.document.getElementsByName("upgrade1")[0]; //Upgrade 1 button
     var currencyText = this.document.getElementsByName("currency")[0]; //The text for the user's currency
     var clicked = 0; //Mainly a debug var but is used for displaying the user's money
     var clickScale = 1; //For if any of the upgrades increase money per click
@@ -9,9 +10,13 @@ window.addEventListener("load", function(){
 
 
     clickerButton.addEventListener("click", function(){ //When clicker is clicked it will put money up
-        clicked++
+        clicked++;
         currencyText.textContent = "$ " + clicked;
         console.log("The button has been clicked!!! We are now at: " + clicked + " Clicks!");
+    })
+
+    upgradeButton1.addEventListener("click", function(){
+        console.log("Upgrade 1 purchased! :) ");
     })
 })
 
