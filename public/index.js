f71fe2f15dc298d83eacd36181d1c0c9cee28c0d
TheJackOfAllTrade <hendersonshamus03@gmail.com>
2024-11-06 14:50:07 +0000

    Added Passive Income
    User presses button
    Number goes up
    User buys upgrade
    Number goes down
    Number then goes up passively

--- public/index.js
+++ public/index.js
@@ -5,9 +5,11 @@ window.addEventListener("load", function(){
     var upgradeButton = this.document.getElementsByClassName("upgradeButton"); //The upgrade buttons
     var upgradeCost = this.document.getElementsByClassName("upgradeCost"); //The cost of each upgrade
     var upgradeName = this.document.getElementsByClassName("upgradeName"); //The name of each upgrade
+    var upgradeIncome = this.document.getElementsByClassName("upgradeIncome") //The rate of income for each upgrade
     var currencyText = this.document.getElementsByName("currency")[0]; //The text for the user's currency
-    var currency = currencyText.textContent.substring(1);
+    var currency = parseInt(currencyText.textContent.substring(1));
     var upgradeOwned = [...Array(upgradeButton.length).fill(0)]; //The amount of each upgrade the user owns
+    var totalRateOfIncome = 0; //The rate of income of every upgrade added up
     var clickScale = 1; //For if any of the upgrades increase money per click
     console.log("Page Loaded Successfully");
     console.log(upgradeOwned);
@@ -29,6 +31,13 @@ window.addEventListener("load", function(){
                 upgradeOwned[i]++;
                 upgradeName[i].textContent = "Upgrade " + (i+1) + " (" + upgradeOwned[i] + ")";
                 console.log("Upgrade Bought!");
+
+                totalRateOfIncome = 0;
+                for(let i=0; i < upgradeIncome.length; i++){
+                    totalRateOfIncome += (upgradeOwned[i] * upgradeIncome[i].textContent);
+                }
+                console.log("Total rate of Income is: " + totalRateOfIncome);
+                
             }
             else{
                 console.log("Upgrade not bought.");
@@ -37,15 +46,23 @@ window.addEventListener("load", function(){
             console.log("The user now owns " + upgradeOwned[i] + " of " + upgradeName[i].textContent);
     })}
 
+    window.setInterval(passiveIncome, 100);
+
 
     function changeCurrency(currency){
         if(currency >= 0){
-        currencyText.textContent = currencyText.textContent = "$ " + currency;
+        currencyText.textContent = "$ " + currency;
         }
         else{
             console.log("Can't put currency below 0");
             return;
         }
+    }
+
+    function passiveIncome(){
+        console.log("Current Currency is: " + currency);
+        currency += (totalRateOfIncome/10);
+        changeCurrency(currency);
     }
 })
 
